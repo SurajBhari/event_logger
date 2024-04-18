8b1396ef629c855d25c5f2010781ffbaeb510778
SurajBhari <surajbhari159@gmail.com>
2024-04-18 08:03:37 +0530

    add location command for admins

--- hook.py
+++ hook.py
@@ -2,6 +2,7 @@ from whatsapp import WhatsApp, Message
 from json import load, loads, dump, dumps
 import logging
 import os
+import pytz
 from time import time
 from whatsapp import WhatsApp, Message
 from flask import Flask, request, Response
@@ -11,11 +12,12 @@ try:
 except FileNotFoundError:
     creds = {}
 
-print(creds)
+from datetime import datetime, tzinfo
+admins = creds['admins']
 messenger = WhatsApp(creds['token'],  phone_number_id=creds['numberid'], logger=True, update_check=True)
 # Initialize Flask App
 app = Flask(__name__)
-
+tz = pytz.timezone('Asia/Kolkata')
 VERIFY_TOKEN = creds['verify-token']
 
 # make and verify data.json
@@ -76,6 +78,12 @@ def hook():
             if message_type == "text":
                 message = msg.content
                 name = msg.name
+                if message in command_dict:
+                    logging.info(
+                        f"Launching command {command_dict[message]}"
+                    )
+                    command_dict[message](data=data)
+                    return "OK", 200
                 logging.info("Message: %s", message)
                 m = Message(instance=messenger, to=mobile,
                             content=message)
@@ -105,7 +113,7 @@ def hook():
                              message_latitude, message_longitude)
                 sdata['locations'].append({
                     'time': int(time()),
-                    'sender': mobile,
+                    'sender': {'mobile':mobile, 'name':msg.name},
                     'location': {
                         'latitude': message_latitude,
                         'longitude': message_longitude
@@ -122,6 +130,41 @@ def hook():
                 logging.info("No new message")
     return "OK", 200
 
+
+
+def handle_location_ask(data:None):
+    if not data:
+        return
+    m = Message(instance=messenger, data=data)
+    if m.sender not in admins:
+        return 
+    u_l = {}
+    today = datetime.today()
+    for location in sdata['locations']:
+        xtime = datetime.fromtimestamp(location['time'], tz=tz)
+        if xtime.date() != today.date():
+            continue
+        try:
+            u_l[location['sender']['mobile']].append(location)
+        except KeyError:
+            u_l[location['sender']['mobile']] = [location]
+    
+    string = ""
+    for mobile in u_l:
+        locations = u_l[mobile]
+        string += f"{locations[0]['sender']['name']} ({mobile})\n"
+        for l in locations:
+            string += f"{datetime.fromtimestamp(l['time'], tz=tz).strftime('%H:%M')} - {l['location']['latitude']}, {l['location']['latitude']}\n"
+        string += '\n\n'
+    if not string:
+        string = "No data"
+    m = Message(instance=messenger, to=m.sender, content=string)
+    m.send()
+    return
+
+command_dict = {
+    'location': handle_location_ask
+}
 
 if __name__ == "__main__":
     context = ('/etc/letsencrypt/live/surajbhari.info/fullchain.pem', '/etc/letsencrypt/live/surajbhari.info/privkey.pem')
